2d897314222e353183960824a1c32fc82edcf1bb
Leandre-B <contact@leandre-b.com>
2025-12-01 22:30:00 +0100

    clean and arrange code for fun

--- 2025/1/main.cpp
+++ 2025/1/main.cpp
@@ -1,29 +1,29 @@
 #include <iostream>
 #include <fstream>
 
+int move(const char & direction, const int & dpl, int & cur){
+    if(direction=='L')
+        cur=((cur - dpl) %100 + 100)%100;
+    else
+        cur=((cur + dpl)%100);
 
-void part2(uint & password, const std::string & mouvement, int & cur){
-    int dpl = std::stoi(mouvement.substr(1));
-    for(int i=0; i<dpl; ++i){
-        if(mouvement[0]=='L')
-            cur=((cur - 1) %100 + 100)%100;
-        else
-            cur=((cur + 1) %100 + 100)%100;
-        if(cur== 0)
-            ++password;
-    }
+    return cur;
 }
 
-
-void part1(uint & password, const std::string & mouvement, int & cur){
+void solve(const char & part, uint & password, const std::string & mouvement, int & cur){
     int dpl = std::stoi(mouvement.substr(1));
-    if(mouvement[0]=='L')
-        cur=((cur - dpl) %100 + 100)%100; //get positive mod
-    else
-        cur=((cur + dpl) %100 + 100)%100; //get positive mod
-    
-    if(cur== 0)
-        ++password;
+
+    if(part=='1'){
+        cur = move(mouvement[0], dpl, cur);
+        if(cur== 0)
+            ++password;
+    }else{ //part === 2
+        for(int i=0; i<dpl; ++i){
+            cur = move(mouvement[0], 1, cur);
+            if(cur== 0)
+                ++password;
+        }
+    }
 }
 
 int main(){
@@ -31,20 +31,24 @@ int main(){
     std::ifstream fic;
     fic.open(file_name);
     if(fic.is_open()){
-        
-        uint password = 0;
-        int cur = 50;
-        std::string mouvement;
-        while(fic.good()){
-            std::getline(fic, mouvement);
-            
-            //part1(password, mouvement, cur);
-            part2(password, mouvement, cur);
+        char part;
+        std::cout<<"What part do you want to solve ? ";
+        std::cin>>part;
+        if(part!='1' and part!='2')
+            std::cout<<"Invalide, enter a proper part value !\n";
+        else{
+
+            uint password = 0;
+            int cur = 50;
+            std::string mouvement;
+            while(fic.good()){
+                std::getline(fic, mouvement);
+                
+                solve(part, password, mouvement, cur);
+            }
+
+            std::cout<<"The password for part "<<part<<" is : "<<password<<"\n";
         }
-
-        std::cout<<"The password is : "<<password<<"\n";
-
-
 
     }else
         std::cout<<"Fail to open \"input.txt\"\n";
